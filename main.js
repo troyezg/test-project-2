d5dabe7afc626d4d0ab2b9387746ff2715c312a5
Nikita Simonov <ngsimonov@yandex.ru>
2024-08-22 23:42:49 +0500

    feat: reviews and where blocks done

---
+++ main.js
@@ -0,0 +1,5 @@
+let DeskButton = document.getElementById("main-header__button-description");
+DeskButton.addEventListener("click", function() {
+    let DeskBlock = document.getElementById("description-block");
+    DeskBlock.className = "description-block section active";
+});
